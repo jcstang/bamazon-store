90b059ce0003cf66d765cbc280dc2dc5d07c7bc5
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-11 09:34:50 -0600

    storing data local

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,9 @@ const figlet = require('figlet');
 const keys = require('./keys');
 const Table = require('cli-table');
 let connection;
-let dataStore;
+// let dataStore;
+// let storeTheStore = [];
+let storeTheStoreNew = [];
 
 // ===================================================
 // START
@@ -42,10 +44,42 @@ function startupStore() {
     console.log("connected as id " + chalk.yellow(connection.threadId));
   });
 
-  connection.query("SELECT item_id, product_name, price FROM bamazon.products;",function(err, data, fields) {
+  connection.query("SELECT item_id, product_name, price, department_name, stock_quantity FROM bamazon.products;",function(err, data, fields) {
     if(err) {
       throw err;
     }
+
+    // dataStore = data;
+    // console.log('datastore');
+    // console.log(dataStore);
+    // console.log(dataStore[0]);
+
+    // data.forEach(element => {
+    //   storeTheStore.push(element);
+    // });
+    storeTheStore = data;
+    // console.log('storeTheStore');
+    // console.log(storeTheStore);
+    storeTheStore.forEach(element => {
+      // console.log(`item_id: ${element.item_id} product: ${element.product_name}`);
+
+      let obj = {
+        id: element.item_id,
+        name: element.product_name,
+        price: element.price,
+        stock: element.stock_quantity,
+        deptName: element.department_name
+
+      }
+      storeTheStoreNew.push(obj);
+      
+    });
+    
+    console.log('storeTheStoreNew');
+    console.log(storeTheStoreNew);
+    
+    
+    
 
     printyPrint(data, fields);
     promptBuyer();
@@ -97,42 +131,9 @@ function promptBuyer() {
 
 }
 
-function getDataPromise(key) {
-  // let qString = `SELECT * FROM bamazon.products WHERE item_id=${key}`
-  // connection.query(qString, (err, data, fields) => {
-  //   if (err) {
-  //     throw err;
-  //   }
-  // });
-
-  // let myFirstPromise = new Promise((resolve, reject) => {
-  //   // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
-  //   // In this example, we use setTimeout(...) to simulate async code. 
-  //   // In reality, you will probably be using something like XHR or an HTML5 API.
-  //   setTimeout( function() {
-  //     resolve("Success!");  // Yay! Everything went well!
-  //   }, 250) 
-  // }) 
-  
-  // return myFirstPromise.then((successMessage) => {
-  //   // successMessage is whatever we passed in the resolve(...) function above.
-  //   // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
-  //   console.log("Yay! " + successMessage);
-  // });
-
-}
 
 function hasStock(key, amtAsking) {
   console.log('made it to hasStock');
-
-  // getDataPromise(key).then(function(res) {
-  //   console.log('promise fulfilled');
-    
-  // });
-  // getDataPromise(key).then(function(res) {
-  //   console.log('returned from getDataPromise');
-    
-  // });
   
   let qString = `SELECT * FROM bamazon.products WHERE item_id=${key}`
   connection.query(qString, function(err, data, fields) {
@@ -190,35 +191,6 @@ function getRecordFromKey(key) {
   });
 
 }
-
-
-// function doesProductHaveEnough(key, quantity) {
-//   console.log('start of doesProductHaveEnough');
-  
-//   let queryString = `SELECT item_id, product_name, stock_quantity FROM bamazon.products WHERE item_id=${key};`;
-//   // console.log(queryString);
-  
-//   connection.query(queryString, function(err, data) {
-//     if (err) {
-//       throw err;
-//     }
-
-//     console.log(data);
-//     console.log(data[0].stock_quantity);
-    
-//     if(quantity > data[0].stock_quantity) {
-//       console.log(chalk.red('there is not enough!!!!!!!!!!!!!!'));
-//       process.exit(1); 
-//     }
-//     fulfillOrder(data[0].item_id, data[0].stock_quantity);
-//     // console.log(data);
-//     // printTable(data);
-//     // process.exit(0);
-//     // return data;
-
-//     console.log('end of doesProductHaveEnough');
-//   });
-// }
 
 
 
