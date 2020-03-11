89a9b8dc050a7d9287613a2cb1991e646542663d
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-11 09:06:21 -0600

    comment out functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -97,27 +97,27 @@ function promptBuyer() {
 }
 
 function getDataPromise(key) {
-  let qString = `SELECT * FROM bamazon.products WHERE item_id=${key}`
+  // let qString = `SELECT * FROM bamazon.products WHERE item_id=${key}`
   // connection.query(qString, (err, data, fields) => {
   //   if (err) {
   //     throw err;
   //   }
   // });
 
-  let myFirstPromise = new Promise((resolve, reject) => {
-    // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
-    // In this example, we use setTimeout(...) to simulate async code. 
-    // In reality, you will probably be using something like XHR or an HTML5 API.
-    setTimeout( function() {
-      resolve("Success!");  // Yay! Everything went well!
-    }, 250) 
-  }) 
+  // let myFirstPromise = new Promise((resolve, reject) => {
+  //   // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+  //   // In this example, we use setTimeout(...) to simulate async code. 
+  //   // In reality, you will probably be using something like XHR or an HTML5 API.
+  //   setTimeout( function() {
+  //     resolve("Success!");  // Yay! Everything went well!
+  //   }, 250) 
+  // }) 
   
-  return myFirstPromise.then((successMessage) => {
-    // successMessage is whatever we passed in the resolve(...) function above.
-    // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
-    console.log("Yay! " + successMessage);
-  });
+  // return myFirstPromise.then((successMessage) => {
+  //   // successMessage is whatever we passed in the resolve(...) function above.
+  //   // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+  //   console.log("Yay! " + successMessage);
+  // });
 
 }
 
